17363803c4071a3a8e941ae855a030a0a19da4fd
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-12-13 21:56:58 +0700

    `fixing` modal close on backdrop

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,17 +2,19 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
-const BackdropOverlay = () => {
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 z-50 backdrop-blur-sm" />
-  );
-};
-
-const ModalOverlay = ({ children, showModal, desktopView, onClose }) => {
+const BackdropOverlay = ({ onClose }) => {
   const handleClose = () => {
     onClose();
   };
+  return (
+    <div
+      className="fixed inset-0 bg-black bg-opacity-50 z-50 backdrop-blur-sm"
+      onClick={handleClose}
+    />
+  );
+};
 
+const ModalOverlay = ({ children, showModal, desktopView }) => {
   const desktopViewClass = `items-center ${
     showModal ? 'opacity-100 scale-100' : 'opacity-0 scale-95'
   }`;
@@ -22,10 +24,9 @@ const ModalOverlay = ({ children, showModal, desktopView, onClose }) => {
 
   return (
     <div
-      className={`fixed inset-0 w-full h-screen flex justify-center z-[100] transition-all duration-300 ease-in-out ${
+      className={`fixed inset-x-0 bottom-0 top-auto w-full h-auto flex justify-center z-[100] transition-all duration-300 ease-in-out ${
         desktopView ? desktopViewClass : mobileViewClass
-      }`}
-      onClick={handleClose}>
+      }`}>
       <div
         className={`bg-white pt-14 pb-5 ${
           desktopView ? 'rounded-xl w-[32rem]' : 'rounded-t-2xl w-full'
@@ -63,12 +64,14 @@ function Modal({ children, onClose }) {
 
   return (
     <>
-      {ReactDOM.createPortal(<BackdropOverlay />, modalRootElement)}
+      {ReactDOM.createPortal(
+        <BackdropOverlay onClose={onClose} />,
+        modalRootElement
+      )}
       {ReactDOM.createPortal(
         <ModalOverlay
           showModal={showModal}
-          desktopView={isDesktopView}
-          onClose={onClose}>
+          desktopView={isDesktopView}>
           {children}
         </ModalOverlay>,
         modalRootElement
@@ -77,11 +80,13 @@ function Modal({ children, onClose }) {
   );
 }
 
+BackdropOverlay.propTypes = {
+  onClose: PropTypes.func.isRequired,
+};
 ModalOverlay.propTypes = {
   children: PropTypes.node.isRequired,
   showModal: PropTypes.bool.isRequired,
   desktopView: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
 };
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
