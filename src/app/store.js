24c30c29cc8005ebf74e248e5c054fdcd984a3ac
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-01-01 12:43:06 +0700

    `refactor` dir structure

---
+++ src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartSlice from '../features/cart/cartSlice';
+import productSlice from '../features/producList/productSlice';
+import filterSlice from '../features/producList/filterSlice';
+import wishlistSlice from '../features/wishlist/wishlistSlice';
+
+export default configureStore({
+  reducer: {
+    product: productSlice,
+    cart: cartSlice,
+    filter: filterSlice,
+    wishlist: wishlistSlice,
+  },
+});
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
-import store from './store';
+import store from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
