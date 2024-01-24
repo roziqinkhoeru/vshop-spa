6a792d83274ad86c54cc17006bab42716e7041a9
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2024-01-24 14:34:59 +0700

    `chore` update format number utils

--- src/utils/numberFormat.js
+++ src/utils/numberFormat.js
@@ -1,7 +1,21 @@
 const dotThree = (number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 
+const formatCurrency = (number) => {
+  const numericValue = parseFloat(number);
+  if (Number.isNaN(numericValue)) {
+    return 0;
+  }
+  const formattedNumber = new Intl.NumberFormat('en-US', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(numericValue);
+
+  return formattedNumber;
+};
+
 const numberFormat = {
   dotThree,
+  formatCurrency,
 };
 
 export default numberFormat;
